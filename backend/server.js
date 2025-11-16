8ed81b8c47b978ad64f7bd0bda0e2d5e48555245
BryanVernon <bryan.vernon@hotmail.com>
2025-11-16 15:03:46 -0600

    troubleshooting auth

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",  // your frontend origin
+  origin: "https://nextgenscores.org",  // your frontend origin
   credentials: true,               // allow cookies to be sent
 }));
 app.use("/api", subscriberRoutes);
